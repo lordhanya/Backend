d265535fe679d431316344807b1b279fa04e43c4
lordhanya <ashifrahman8638471722@gmail.com>
2024-10-27 21:07:39 +0530

    connectionLimit removed and added connectionTimeout

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.PORT || 5000,
-    connectionLimit: 10,
+    connectionTimeout: 50000,
     ssl: {rejectUnauthorized: false}
 });
 
