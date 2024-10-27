c4841032361b446fb0c6634cd186f383d6d38141
lordhanya <ashifrahman8638471722@gmail.com>
2024-10-27 20:39:13 +0530

    port changed

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 3306,
+    port: process.env.PORT || 5000,
     connectTimeout: 50000,
     ssl: {rejectUnauthorized: false}
 });
