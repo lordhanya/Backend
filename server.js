3c2b35c851f7a4675c1915ccd103171ece88fac1
lordhanya <ashifrahman8638471722@gmail.com>
2024-11-13 13:13:34 +0530

    const PORT = DB_PORT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const moment = require('moment-timezone');
 
 const app = express();
-const PORT = process.env.LOCAL_PORT; // Use PORT from .env
+const PORT = process.env.DB_PORT || 5000; // Use PORT from .env
 
 // Middleware
 app.use(cors());
@@ -21,7 +21,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    // port: process.env.DB_PORT,
+    port: process.env.DB_PORT,
     // ssl: {rejectUnauthorized: false}
 });
 
